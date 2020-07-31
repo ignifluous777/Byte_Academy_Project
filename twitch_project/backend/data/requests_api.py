ab34337ac62e1e33e5d9dd6f4301e2123af979c1
Jonathan Gayfield <jongayfield@gmail.com>
2020-07-31 15:02:26 -0400

    both api endpoints work & print to .json files

--- twitch_project/backend/data/requests_api.py
+++ twitch_project/backend/data/requests_api.py
@@ -13,11 +13,19 @@ endpoint_user = f"https://api.twitch.tv/kraken/users?login={user_login}"
 endpoint_follows = f"https://api.twitch.tv/kraken/users/{user_id}/follows/channels"
 
 def get_user():
-    response = requests.get(endpoint, headers=headers)
-    # pprint(response.content)
-    # print(response.status_code)
-    # print(response.headers)
-    pprint(response.json())
+
+    resp1 = requests.get(endpoint_user, headers=headers)
+    resp2 = requests.get(endpoint_follows, headers=headers)
+
+    with open('user_data.json', 'w') as file_object:
+        json.dump(resp1.json(), file_object, indent=4)
+    
+    with open('follows_data.json', 'w') as file_object:
+        json.dump(resp2.json(), file_object, indent=4)
+
+    # print(resp1.status_code)
+    pprint(resp1.json())
+    pprint(resp2.json())
     return
 
 get_user()
